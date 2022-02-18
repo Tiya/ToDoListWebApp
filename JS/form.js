0ba20a7b1b68b1b62d0e1a5867d31525221626b8
Tiya <tiyamartin@gmail.com>
2022-02-18 12:06:22 +0530

    fixed

--- JS/form.js
+++ JS/form.js
@@ -5,11 +5,10 @@ let error_n= document.getElementById("error_n");
  
 let regname='admin';
 let regpwd=12345;
-let flag=0;
 
 function validateForm(){
  
-    let username= document.getElementById("username").toLowerCase();
+    let username= document.getElementById("username");
     let pwd= document.getElementById("pwd");
     validate_login(username, pwd, myCallbackFuntion);
     return false;
@@ -21,25 +20,21 @@ function validate_login(username, pwd, callback){
     if(username.value==""){
         error_e.innerHTML="Please enter your username";
         error_e.style.color="red";
-        flag=0;
-      //  return false;
+        return false;
     }else if(!(regname==username.value)){
-        error_e.innerHTML="Incorrect Username!!";
+        error_e.innerHTML="Incorrect Username!! username is case-sensitive";
         error_e.style.color="red";
-        flag=0;
-      //  return false;
+        return false;
     }else if(pwd.value==""){
         error_e.innerHTML="";
         error_p.innerHTML="Please enter your password";
         error_p.style.color="red";
-        flag=0;
-     //   return false;
+        return false;
     }else if(!(regpwd==pwd.value)){
         error_e.innerHTML="";
         error_p.innerHTML="Incorrect Password";
         error_p.style.color="red";
-        flag=0;
-     //   return false;
+        return false;
     }else{
        callback();
       
@@ -48,7 +43,7 @@ function validate_login(username, pwd, callback){
 }
 //************callback***********
 function myCallbackFuntion(){
-  alert("suucessfully logged in!!!")
+  alert("sucessfully logged in!!!")
   window.location.href="todolist.html";
 }
 
